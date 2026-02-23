f456e420d08ffda2e6da07fc818720dd07641861
Manu_Love <107737902+mloveral@users.noreply.github.com>
2026-02-23 16:38:14 -0500

    planeacion

--- app/descubrir/page.tsx
+++ app/descubrir/page.tsx
@@ -1,8 +1,14 @@
+import { CategoriesCard } from "@/components/products/categories-card";
 
 export default function DiscoveryPage() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex gap-7 justify-begin p-12 w-full">
+            <div className="flex flex-col col-span-7">
+                <CategoriesCard />
+            </div>
+            <div className=" flex flex-col col-span-full">
+                
+            </div>
     </div>
   );
 }
