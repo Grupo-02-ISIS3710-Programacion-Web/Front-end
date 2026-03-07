d96c3d7c3598ff7efb743fde02db44017a052926
mateo-1046 <m.canteg@uniandes.edu.co>
2026-03-07 06:18:17 -0500

    Home

--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -7,28 +7,25 @@ export default function Home() {
 
       <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-8 items-center">
 
-        {/* TEXTO */}
         <div>
-
-          {/* badge */}
+        
           <span className="bg-secondary text-xs px-3 py-1 rounded-full font-medium text-gray-700">
             CUIDADO DE LA PIEL RESPALDADO POR EXPERTOS
           </span>
 
-          {/* titulo */}
           <h1 className="mt-4 text-5xl font-bold leading-tight text-gray-900">
             Tu viaje de <br />
             <span className="text-primary">Skincare</span> empieza aquí
           </h1>
 
-          {/* descripcion */}
+        
           <p className="mt-6 text-gray-600 max-w-lg">
             Únete a una comunidad de más de 50 mil entusiastas. 
             Haz seguimiento a tu progreso, encuentra la rutina perfecta
             para tu tipo de piel y descubre productos que realmente funcionan.
           </p>
 
-          {/* botón + usuarios */}
+       
           <div className="flex items-center gap-6 mt-8">
 
             <button className="flex items-center gap-2 bg-primary hover:bg-pink-600 text-white px-6 py-3 rounded-xl shadow-md transition">
@@ -36,7 +33,7 @@ export default function Home() {
               <ArrowRight size={18}/>
             </button>
 
-            {/* avatares */}
+           
             <div className="flex items-center gap-3">
 
               <div className="flex -space-x-3">
