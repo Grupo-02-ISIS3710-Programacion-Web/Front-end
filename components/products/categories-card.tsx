9c3c9e85fcdef806c9c9e52dccb30c822a822117
DiegoMunevar2007 <d.munevar@uniandes.edu.co>
2026-03-17 16:24:46 -0500

    Github Actions

--- components/products/categories-card.tsx
+++ components/products/categories-card.tsx
@@ -5,24 +5,24 @@ import { Card, CardHeader, CardContent, CardTitle, CardDescription } from "../ui
 import { Button } from "../ui/button";
 import { ButtonGroup } from "../ui/button-group";
 import {
-  Droplets,
-  LayoutDashboard,
-  Pipette,
-  SoapDispenserDroplet,
-  Sparkles,
-  SprayCan,
-  Sun
+    Droplets,
+    LayoutDashboard,
+    Pipette,
+    SoapDispenserDroplet,
+    Sparkles,
+    SprayCan,
+    Sun
 } from "lucide-react";
 
 import { Category } from "@/types/product";
 import { useTranslations } from "next-intl";
 
 interface CategoriesCardProps {
-  currentCategory?: Category | "ALL";
+    currentCategory?: Category | "ALL";
 }
 
 interface CategoriesCardDesktopMobileProps {
-  isCategorySelected: (category: Category | "ALL") => boolean;
+    isCategorySelected: (category: Category | "ALL") => boolean;
 }
 
 const categories = [
@@ -43,13 +43,13 @@ export function CategoriesCard({ currentCategory }: CategoriesCardProps) {
 
     return (
         <>
-        <div className="hidden lg:block">
-            <CategoriesCardDesktop isCategorySelected={isSelected} />
-        </div>
+            <div className="hidden lg:block">
+                <CategoriesCardDesktop isCategorySelected={isSelected} />
+            </div>
 
-        <div className="block lg:hidden">
-            <CategoriesCardMobile isCategorySelected={isSelected} />
-        </div>
+            <div className="block lg:hidden">
+                <CategoriesCardMobile isCategorySelected={isSelected} />
+            </div>
         </>
     );
 }
@@ -61,79 +61,77 @@ export function CategoriesCardDesktop({ isCategorySelected }: CategoriesCardDesk
 
     return (
         <div className="px-1">
-        <Card className="mx-auto px-0">
-            <CardHeader>
-                <CardTitle>
-                    {t("title")}
-                </CardTitle>
-                <CardDescription>
-                    {t("description")}
-                </CardDescription>
-            </CardHeader>
+            <Card className="mx-auto px-0">
+                <CardHeader>
+                    <CardTitle>
+                        {t("title")}
+                    </CardTitle>
+                    <CardDescription>
+                        {t("description")}
+                    </CardDescription>
+                </CardHeader>
 
-            <CardContent>
-                <Stack direction={"column"}>
-                    <ButtonGroup orientation="vertical" className="h-full w-full">
-                    {categories.map((category, index) => (
-                        <Button
-                        key={index}
-                        className={`flex justify-baseline w-full hover:bg-secondary py-5 ${
-                            isCategorySelected(category.value)
-                            ? "bg-secondary text-secondary-foreground"
-                            : "bg-muted/20 text-foreground"
-                        }`}
-                        asChild
-                        >
-                        <a
-                            href={category.value === "ALL"
-                            ? "/descubrir"
-                            : `?category=${category.value}`}
-                            className="flex justify-baseline gap-2"
-                        >
-                            {category.icon}
-                            <div>
-                            {tCat(`${category.value}.label`)}
-                            </div>
-                        </a>
-                        </Button>
-                    ))}
-                    </ButtonGroup>
-                </Stack>
-            </CardContent>
-        </Card>
+                <CardContent>
+                    <Stack direction={"column"}>
+                        <ButtonGroup orientation="vertical" className="h-full w-full">
+                            {categories.map((category, index) => (
+                                <Button
+                                    key={index}
+                                    className={`flex justify-baseline w-full hover:bg-secondary py-5 ${isCategorySelected(category.value)
+                                            ? "bg-secondary text-secondary-foreground"
+                                            : "bg-muted/20 text-foreground"
+                                        }`}
+                                    asChild
+                                >
+                                    <a
+                                        href={category.value === "ALL"
+                                            ? "/descubrir"
+                                            : `?category=${category.value}`}
+                                        className="flex justify-baseline gap-2"
+                                    >
+                                        {category.icon}
+                                        <div>
+                                            {tCat(`${category.value}.label`)}
+                                        </div>
+                                    </a>
+                                </Button>
+                            ))}
+                        </ButtonGroup>
+                    </Stack>
+                </CardContent>
+            </Card>
         </div>
     );
 }
 
 export function CategoriesCardMobile({ isCategorySelected }: CategoriesCardDesktopMobileProps) {
 
-    const t = useTranslations("CategoriesCard");
+    const tCat = useTranslations("Categories");
 
     return (
         <div className="px-1 flex justify-begin gap-1 overflow-auto">
-        {categories.map((category, index) => (
-            <Button
-            key={index}
-            className={`flex justify-baseline w-fit hover:bg-secondary py-5 ${
-                isCategorySelected(category.value)
-                ? "bg-secondary text-secondary-foreground"
-                : "bg-muted text-foreground"
-            }`}
-            asChild
-            >
-            <a
-                href={category.value === "ALL"
-                ? "/descubrir"
-                : `?category=${category.value}`}
-                className="flex justify-baseline gap-2"
-            >
-                {category.icon}
-                <div>
-                {t(`categories.${category.value}.label`)}
-                </div>
-            </a>
-            </Button>
-        ))}
+            {categories.map((category, index) => (
+                <Button
+                    key={index}
+                    className={`flex justify-baseline w-fit hover:bg-secondary py-5 ${isCategorySelected(category.value)
+                            ? "bg-secondary text-secondary-foreground"
+                            : "bg-muted text-foreground"
+                        }`}
+                    asChild
+                >
+                    <a
+                        href={category.value === "ALL"
+                            ? "/descubrir"
+                            : `?category=${category.value}`}
+                        className="flex justify-baseline gap-2"
+                    >
+                        {category.icon}
+                        <div>
+                            {tCat(`${category.value}.label`)}
+                        </div>
+                    </a>
+                </Button>
+            ))}
 
         </div>
     );
