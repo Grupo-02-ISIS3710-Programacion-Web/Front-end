f08dfbb999a02d907a52dc08eb1c7b6257d15fef
Manu_Love <107737902+mloveral@users.noreply.github.com>
2026-03-17 20:14:30 -0500

    config

---
+++ Jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+}
+
+export default createJestConfig(config)
---
+++ Jest.setup.ts
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+// Silencia errores de consola en los tests (opcional, comenta si los necesitas)
+// beforeAll(() => {
+//   jest.spyOn(console, 'error').mockImplementation(() => {})
+//   jest.spyOn(console, 'warn').mockImplementation(() => {})
+// })
+
+// afterAll(() => {
+//   jest.restoreAllMocks()
+// })
